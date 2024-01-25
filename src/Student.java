9c3bdc5978123bc38b36e8ddcfa322d7114e9ccd
suyundykovv <230639@astanait.edu.kz>
2024-01-25 23:01:33 +0600

    writed private and super for Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,12 @@
-public class Student {
-}
+class Student extends Person {
+    private double gpa;
+    private static final double STIPEND = 36660.00;
+
+    public Student() {
+        super();
+    }
+
+    public Student(String name, String surname, double gpa) {
+        super(name, surname);
+        this.gpa = gpa;
+    }

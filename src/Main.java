0793443255fb99a496aeffe3a72e80c0ea642ebf
suyundykovv <230639@astanait.edu.kz>
2024-01-25 23:23:21 +0600

    finished main and all code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
-
 import java.util.ArrayList;
+import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("Azamat", 0);
-        Employee employee = new Employee("Runar", 5);
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Employee("Nursultan", "Khaimuldin", "Professor", 400000.78));
+        people.add(new Employee("kassym-Jomart", "Tokaev", "President", 690000.00));
+        people.add(new Student("Tyryon", "Lannister", 4.00));
+        people.add(new Student("Margulan", "Suyundykov", 2.30));
 
-        ArrayList<Person> persons = new ArrayList<>();
-        persons.add(student);
-        persons.add(employee);
-        persons.add();
+        Collections.sort(people);
 
-
-
+        printData(people);
     }
+
+    public static void printData(Iterable<Person> people) {
+        for (Person person : people) {
+            System.out.println(person.toString());
+        }
     }
+}

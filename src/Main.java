3e3e76773e8124287c243b492c375019d71a6fe9
suyundykovv <230639@astanait.edu.kz>
2024-01-25 22:37:38 +0600

    Created classes Person, Employee and Student

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Azamat", 0);
+        Employee employee = new Employee("Runar", 5);
+
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(student);
+        persons.add(employee);
+        persons.add();
+
+
+
+    }
+    }

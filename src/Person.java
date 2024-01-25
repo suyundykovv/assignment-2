4910d475f40e950e68a77bd7d7245c502415e77c
suyundykovv <230639@astanait.edu.kz>
2024-01-25 22:49:11 +0600

    write the getters and setter of private methods

--- src/Person.java
+++ src/Person.java
@@ -13,3 +13,30 @@ class Person implements Comparable<Person>, Payable {
         this.name = name;
         this.surname = surname;
     }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String toString() {
+        return String.format("%s: %d. %s %s", getPosition(), id, name, surname);
+    }
+
+    public String getPosition() {
+        return "Person";
+    }

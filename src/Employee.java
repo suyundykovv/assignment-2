103af71489d1d23de33eb35f84dda9cba67b506d
suyundykovv <230639@astanait.edu.kz>
2024-01-25 23:08:41 +0600

    write public and super for Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,13 @@
-public class Employee {
-}
+class Employee extends Person {
+    private String position;
+    private double salary;
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(String name, String surname, String position, double salary) {
+        super(name, surname);
+        this.position = position;
+        this.salary = salary;
+    }
